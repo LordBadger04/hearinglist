77106ec9d79de2e782f601ea319d98e27370a970
rachid moutawakel <rachid.moutawakel.dev@gmail.com>
2026-08-18 15:39:51 +0200

    Add AI chat with RubyLLM

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,11 +1,19 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
-    
+    @chat = Chat.new(title: "Untitled")
+    @chat.user = current_user
+
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      redirect_to root_path, alert: "Unable to create chat."
+    end
   end
 
   def show
-
+    @chat = current_user.chats.find(params[:id])
+    @message = Message.new
   end
-
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,41 @@
 class MessagesController < ApplicationController
-def create
+  before_action :authenticate_user!
 
-end
+  SYSTEM_PROMPT = <<~PROMPT
+    You are a music assistant for hearinglist.
 
+    Help users discover songs, artists and live cover versions.
 
+    Answer clearly and concisely.
+  PROMPT
 
+  def create
+    @chat = current_user.chats.find(params[:chat_id])
+
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      response = RubyLLM.chat
+        .with_instructions(SYSTEM_PROMPT)
+        .ask(@message.content)
+
+      Message.create(
+        role: "assistant",
+        content: response.content,
+        chat: @chat
+      )
+
+      redirect_to chat_path(@chat)
+    else
+      render "chats/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
