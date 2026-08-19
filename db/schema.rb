d2e2d817aadf09ed3b06b848a007fdb342477c8d
Florian Durand <florian.grebotdurand@gmail.com>
2026-08-19 12:00:52 +0200

    updated FrontEnd for the list Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_18_153615) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_19_090743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_18_153615) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "image_url"
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
