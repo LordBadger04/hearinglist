08f734d522d3aefbcf132b3d13aa451d5c4c519f
clementbau <bau.clement04@gmail.com>
2026-08-20 16:36:29 +0200

    Fixed dependent destroy

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :versions, through: :bookmarks
   has_one_attached :photo
   # Deux users peuvent avoir une liste "Covers", mais pas le meme user deux fois.
